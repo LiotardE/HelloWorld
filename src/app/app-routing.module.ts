31b934511f224d6fd9812c1396e2841077f5ce45
LiotardE <estelle.liotard@yahoo.fr>
2021-07-30 14:47:09 +0200

    homepage et route (supp titre et remplacement dedans)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarsListComponent } from './cars-list/cars-list.component';
 import { DriversListComponent } from './drivers-list/drivers-list.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
+  { path: "" , component: HomePageComponent },
   { path: "cars" , component: CarsListComponent },
-  { path: 'drivers' , component:DriversListComponent },
+  { path: 'drivers' , component:DriversListComponent }
 ];
 
 @NgModule({
