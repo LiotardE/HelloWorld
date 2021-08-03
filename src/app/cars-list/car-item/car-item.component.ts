1d833fb9263de5c8b7107f0dfdcbec26c88a2378
LiotardE <estelle.liotard@yahoo.fr>
2021-08-03 12:37:46 +0200

    enfant car-item component

--- src/app/cars-list/car-item/car-item.component.ts
+++ src/app/cars-list/car-item/car-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-car-item',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class CarItemComponent implements OnInit {
 
   vehicule:string = "ferrari";
+
+  @Input() enfant:any;
 
   constructor() { }
 
--- src/app/cars-list/cars-list.component.ts
+++ src/app/cars-list/cars-list.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarsListComponent implements OnInit {
 
+  enfantUn:string = "Andreax";
+
+  enfantDeux:string = "Fille";
+
+  enfantTrois:string = "Garçon";
+
+
+
   constructor() { }
 
   ngOnInit(): void {
