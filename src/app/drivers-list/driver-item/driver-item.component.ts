b42b48df2e787e2e750fc775be813038f21a5827
LiotardE <estelle.liotard@yahoo.fr>
2021-08-09 11:58:38 +0200

    insertion input dans import drivers.ts

--- src/app/drivers-list/driver-item/driver-item.component.ts
+++ src/app/drivers-list/driver-item/driver-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-driver-item',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./driver-item.component.css']
 })
 export class DriverItemComponent implements OnInit {
+
+  @Input() driver:any;
 
   constructor() { }
 
