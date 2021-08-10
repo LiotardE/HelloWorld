5aea141155b1b7b6206b87db81703050695ff9a4
LiotardE <estelle.liotard@yahoo.fr>
2021-08-10 15:59:57 +0200

    objet cars dans data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,6 +9,66 @@ export class DataService {
 
   test2:string = 'HAAAA';
 
+  drivers: any = [
+    {
+      fullName: "ken block",
+      pays: "usa",
+      coverImage: "./assets/img/drivers/kenblock.jpg",
+      category: "gymkhana",
+      likeIts: -1
+    },
+    {
+      fullName: "danica patrick",
+      pays: "usa",
+      coverImage: "./assets/img/drivers/danicapatrick.jpg",
+      category: "nascar",
+      likeIts: 0
+    },
+    {
+      fullName: "sebastien loeb",
+      pays: "france",
+      coverImage: "./assets/img/drivers/Loeb.jpg",
+      category: "rallye",
+      likeIts: 1
+    },
+    {
+      fullName: "molly taylor",
+      pays: "australie",
+      coverImage: "./assets/img/drivers/mollytaylor.jpg",
+      category: "rallye",
+      likeIts: -1
+    },
+    {
+      fullName: "collin mcrae",
+      pays: "écosse",
+      coverImage: "./assets/img/drivers/colin.jpg",
+      category: "rallye",
+      likeIts: 0
+    },
+    {
+      fullName: "shirley muldowney",
+      pays: null,
+      coverImage: "./assets/img/drivers/shirleymuldowney.jpg",
+      category: "drag",
+      likeIts: 0
+    },
+    {
+      fullName: "michael schumacher",
+      pays: "allemagne",
+      coverImage: "./assets/img/drivers/schumacher.jpg",
+      category: "formule 1",
+      likeIts: 0
+    },
+    {
+      fullName: "erica enders",
+      pays: "usa",
+      coverImage: "./assets/img/drivers/ericaenders.jpg",
+      category: "drag",
+      likeIts: 0
+    },
+    
+  ];
+
   constructor() {
     console.log(this.test2);
    }
@@ -16,5 +76,9 @@ export class DataService {
   getTest(){
     return this.test;
 }
+
+  getAllCars(){}
+
+  getAllDrivers(){}
 
 }
