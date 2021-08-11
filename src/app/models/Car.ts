6cba6a51f88c33083f37a6653b4b158ab89aa626
LiotardE <estelle.liotard@yahoo.fr>
2021-08-11 16:02:00 +0200

    class Car typeScript plus court

--- src/app/models/Car.ts
+++ src/app/models/Car.ts
@@ -4,20 +4,11 @@
 
  export class Car {
 
-    public name:string;
-    public pays:string;
-    public coverImage:string;
-    public power:number;
-    public perf:number;
 
-    constructor(name:string,pays:string,coverImage:string,power:number,perf:number) {
-        this.name = name;
-        this.pays = pays;
-        this.coverImage = coverImage;
-        this.power = power;
-        this.perf = perf;
-        
-    }
+    constructor(public name:string, 
+                public pays:string,
+                public coverImage:string,
+                public power?:any,
+                public perf?:any ){}
 
-
-}
+} 
