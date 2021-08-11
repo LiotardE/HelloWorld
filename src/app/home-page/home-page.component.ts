0f0846dfd2d223fb3551e0c4f41a2679a64a620d
LiotardE <estelle.liotard@yahoo.fr>
2021-08-11 11:03:05 +0200

    data service fonctionnel

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { BananeService } from '../services/banane.service';
 import { DataService } from '../services/data.service';
 
 
-import { RestaurantService } from '../services/restaurant.service';
+
 
 @Component({
   selector: 'app-home-page',
@@ -14,17 +13,10 @@ export class HomePageComponent implements OnInit {
 
   title:string = "Drive-X";
 
-  fruit:string = this.banana.getFruit();
-
-  prixResto:number = this.resto.getPrice();
-
-  constructor(private banana:BananeService, 
-              private resto:RestaurantService,
-              private data:DataService
-              ) { 
-    console.log(banana.getFruit())
-    console.log(this.resto.name)
-    console.log(this.data.test)
+ 
+ 
+  constructor(private data:DataService) { 
+    console.log(this.data.test);
 
   }
 
