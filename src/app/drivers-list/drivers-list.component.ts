dfb4f4f273601a82edfc42c66a5ebfadd4bf6ac5
LiotardE <estelle.liotard@yahoo.fr>
2021-08-11 10:33:22 +0200

    update récup

--- src/app/drivers-list/drivers-list.component.ts
+++ src/app/drivers-list/drivers-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
 
+
 @Component({
   selector: 'app-drivers-list',
   templateUrl: './drivers-list.component.html',
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BananeService } from '../services/banane.service';
 import { DataService } from '../services/data.service';
+
 import { RestaurantService } from '../services/restaurant.service';
 
 @Component({
