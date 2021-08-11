67a5e75d87ccb9ebdb7dced92dd4fc1bd03bc661
LiotardE <estelle.liotard@yahoo.fr>
2021-08-11 09:59:13 +0200

    driver-item t.s

--- src/app/drivers-list/driver-item/driver-item.component.ts
+++ src/app/drivers-list/driver-item/driver-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-driver-item',
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DriverItemComponent implements OnInit {
 
+  @Input() driver:any;
+
+  //isLiked:boolean = false;
+  //isUnliked:boolean = false;
+
+
+ // likingMessage: string = "Liker maintenant !";
+ // unlikingMessage: string = "UnLiker maintenant !";
+ 
+  
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onLiking(){
+    this.driver.likeIts++;
+    //this.isLiked= true;
+    //this.likingMessage= "Un like en plus ! [liké]";
+    
+  }
+  onUnliked(){
+    this.driver.likeIts--;
+    //this.isUnliked= true;
+    //this.unlikingMessage= "Un like en moins ! [unliké]";
+  }
+
+  
 
 }
