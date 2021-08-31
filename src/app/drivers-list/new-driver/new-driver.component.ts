14471d15829aa1100c7c732ebe02107ba76610c3
LiotardE <estelle.liotard@yahoo.fr>
2021-08-31 14:13:31 +0200

    createForm() + onSubmit()

--- src/app/drivers-list/new-driver/new-driver.component.ts
+++ src/app/drivers-list/new-driver/new-driver.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
-
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Driver } from 'src/app/models/Driver';
 import { DataService } from 'src/app/services/data.service';
 
 @Component({
@@ -10,11 +9,35 @@ import { DataService } from 'src/app/services/data.service';
   styleUrls: ['./new-driver.component.css']
 })
 export class NewDriverComponent implements OnInit {
+  
+  driverForm!: FormGroup;
 
-  constructor(private data:DataService) { }
+  constructor(private data:DataService,
+              private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
-    //this.data.addDriver(this.pilote);
+    this.createForm();
+  }
+
+  createForm(){
+    this.driverForm = this.formBuilder.group({
+      fullName: [''],
+      pays: [''],
+      coverImage: [''],
+      category: ['']
+    })
+  }
+
+  onSubmit(){
+    const formValue = this.driverForm.value;
+    const driver = new Driver(
+      formValue['fullName'],
+      formValue['pays'],
+      formValue['coverImage'],
+      formValue['category']
+    )
+    console.log(driver);
   }
 
 }
+ 
